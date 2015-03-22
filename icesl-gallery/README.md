089dbfb60e901bc786d851898953e76ffffc9273
Loïc Fejoz <loic@fejoz.net>
2015-03-22 18:01:58 +0100

    Add IceSL gallery with twisted model.

---
+++ icesl-gallery/README.md
@@ -0,0 +1,23 @@
+IceSL Gallery
+=============
+
+A Gallery of some designs or trials made with [IceSL](http://www.loria.fr/~slefebvr/icesl/).
+
+Source
+------
+
+Download the [IceSL source or fork on github](https://github.com/loic-fejoz/loic-fejoz-fabmoments/tree/master/icesl-gallery).
+
+See other of [my FabMoments](https://github.com/loic-fejoz/loic-fejoz-fabmoments/tree/master/).
+
+License
+-------
+
+Photos and Screenshots are under the [Creative Commons Attribution 3.0 France](https://creativecommons.org/licenses/by/3.0/fr/) (CC BY 3.0 FR - Loïc Fejoz).
+
+IceSL Models are under the [MIT License](http://opensource.org/licenses/MIT).
+
+Gallery
+-------
+
+![A screenshot of the twisted torus](twisted-0000.png?raw=true)
---
+++ icesl-gallery/twisted.lua
@@ -0,0 +1,47 @@
+--[[
+The MIT License (MIT)
+
+Copyright (c) 2015 Loïc Fejoz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+
+-- Directely applied from http://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
+h = 20
+twisted = implicit(v(-h,-h,0), v(h,h,2*h), [[
+float minDistanceSphereTracing=0.001;
+float distanceEstimator(vec3 ppp)
+{
+  // translate (0,0,18)
+  vec3 pp = ppp - vec3(0,0,18);
+  // Twist p to pp
+  float c = cos(radians(20.0 * pp.z));
+  float s = sin(radians(20.0 * pp.z));
+  mat2 m = mat2(c, -s, s, c);
+  vec3 p = vec3(m * pp.xy, pp.z);
+  // Torus
+  vec2 t = vec2(12.0, 6.0);
+  vec2 q = vec2(length(p.xz)-t.x, p.y);
+  return length(q)-t.y;
+}
+]])
+emit(twisted)
+e = 1
+emit(translate(0, 0, e/2 + 0.220) * box(20, 20, e)) -- the basement
+-- emit(translate(0, 0, 0.220/2) * box(20, 20, 0.220), 3) -- for slow first layer printing
+-- emit(cylinder(30, 0.220), 4) -- for contouring
