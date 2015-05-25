63d4164929f5ba2b9cd4d681212549a6f0513d9f
Loïc Fejoz <loic@fejoz.net>
2015-05-25 17:02:11 +0200

    Set as cut with a scaling factor of 1/4.

--- lampshade/profil-00.lua
+++ lampshade/profil-00.lua
@@ -1,6 +1,7 @@
 dofile 'lampshade.lua'
 
 local config = {
+   scale = 0.25,
    tolerance = 1,
    thickness = 3,
    profile_number = 4, -- shall be 20 to be nicer
@@ -17,6 +18,6 @@ local config = {
       -- radius = at middle
    },
    --   radius = average of the other two radii
---   view = 'cutting' -- could be 'assembled', 'cutting', or absent
+   view = 'cutting' -- could be 'assembled', 'cutting', or absent
 }
 build_lampshade(Path .. 'profil-00.svg', config)
