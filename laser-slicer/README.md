12f3e18ee3c7f0cdd783cf7d3be86e9dee6597e5
Loïc Fejoz <loic.fejoz@realtimeatwork.com>
2018-07-25 18:47:49 +0200

    feat(laser-slicer): Add a script to slice mesh for laser cutting

---
+++ laser-slicer/README.md
@@ -0,0 +1,40 @@
+Laser Slicer
+============
+
+A module for [IceSL](http://www.loria.fr/~slefebvr/icesl/) so as to slice a shape into cardboard or other material for further laser-cutting.
+
+Usage
+-----
+
+1. Copy the `laser-slicer.lua` script into your directory
+2. Copy and rename the `Minion_Dave.lua` into your directory
+3. Adjust the `cfg.board.height` to be the thickness of your cardboard in mm.
+4. Adjust the `cfg.stick.diameter` to ...the diameter of the your stick.
+5. If the stick is not well placed or you want more, mimicks `bear.lua` to provide your owns.
+6. Once satisfied, change the second parameter of `laserslicer.defaultCfg()` to true so as to place slices flat.
+7. Change IceSL service to "Slice to SVG".
+8. Adjust slice thickness so as to have only one slice
+9. Export SVG
+10. Edit SVG so as to keep proper dimmensions
+    1. Look at `viewBox` attribute, eg `viewBox="0.45 -215.05 286.00004 141.116"`
+    2. Add attribute `width` with the 3rd value and appending `mm`, eg `width="286.00004mm"`
+    3. Add atribute `height` with the 4th value, eg `height="141.116mm"`
+
+License
+-------
+
+Photos are under the [Creative Commons Attribution 3.0 France](https://creativecommons.org/licenses/by/3.0/fr/) (CC BY 3.0 FR - Loïc Fejoz).
+
+IceSL Model is under the [MIT License](http://opensource.org/licenses/MIT).
+
+[STL model of the bear by christophecaran](https://www.thingiverse.com/thing:192392/files) is licensed under the [Creative Commons - Attribution - Non-Commercial license](https://creativecommons.org/licenses/by-nc/3.0/).
+
+Gallery
+-------
+
+![Original Bear by christophecaran is licensed under the Creative Commons - Attribution - Non-Commercial license](shot0000.png?raw=true)
+![Coarse-Slices of the bear with sticks](shot0001.png?raw=true)
+![Ready to cut slices](shot0002.png?raw=true)
+![SVG for 2.5mm board thickness and 1.9mm sticks](ours-laser-2.5mm.svg?raw=true)
+![SVG for 3mm board thickness and 1.9mm sticks](ours-laser-3mm.svg?raw=true)
+![Original Minion Dave by Poh is licensed under the GNU - GPL license](shot0003.png?raw=true)
---
+++ laser-slicer/laser-slicer.lua
@@ -0,0 +1,96 @@
+--[[
+The MIT License (MIT)
+
+Copyright (c) 2018 Loïc Fejoz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+local laserslicer = {}
+
+function laserslicer.defaultCfg(the_mesh, cut_or_display)
+   local cfg = {}
+   cfg.cut = cut_or_display
+   cfg.shape = the_mesh
+   cfg.board = {}
+   cfg.board.height = 1
+   cfg.stick = {}
+   cfg.stick.diameter = 2
+   cfg.stick.show = true and not cfg.cut
+   cfg.stick.shape = nil
+   cfg.spaces = {}
+   cfg.spaces.x = 2
+   cfg.spaces.y = 2
+   cfg.dz = 0.001
+   return cfg
+end
+
+function laserslicer.slice(cfg)
+   local b = bbox(cfg.shape)
+   cfg.nb_slices = math.floor(b:max_corner().z / cfg.board.height)
+   print("#slices: " .. cfg.nb_slices)
+   print("size: " .. (cfg.board.height * cfg.nb_slices) .. "mm")
+
+   cfg.dx = b:max_corner().x - b:min_corner().x
+   cfg.dy = b:max_corner().y - b:min_corner().y
+   cfg.dz = 0.001
+   local basic_slice = cube(cfg.dx, cfg.dy, cfg.dz)
+
+   local sticks
+   if cfg.stick.shape == nil then
+      sticks = cylinder(cfg.stick.diameter / 2, b:max_corner().z - b:min_corner().z + cfg.board.height)
+   else
+      sticks = cfg.stick.shape
+   end
+   if cfg.stick.show then
+      emit(sticks, 2)
+   end
+
+   slices = {}
+   for idx=1,cfg.nb_slices do
+      local s = intersection(cfg.shape, translate(0, 0, cfg.board.height * idx) * basic_slice)
+      local slice_scale = cfg.board.height / cfg.dz
+      s = translate(0, 0, cfg.board.height * idx) * scale(1, 1, slice_scale) * translate(0, 0, -cfg.board.height * idx) * s
+      slices[idx] = difference(s, sticks)
+      if not cfg.cut then
+	 emit(slices[idx])
+      end
+   end
+
+   -- 32cm x 19cm
+   if cfg.cut then
+      s_x = 0
+      s_y = 0
+      max_y = 0
+      for idx=1,cfg.nb_slices do
+	 if (s_x > 275) then
+	    s_x = 0
+	    s_y = s_y - max_y + cfg.spaces.y
+	    max_y = 0
+	 end
+
+	 local s = slices[idx]
+	 local b = bbox(s)
+	 s = translate(s_x, s_y, -cfg.board.height * idx) * s
+	 s_x = s_x +  b:max_corner().x -b:min_corner().x + cfg.spaces.x
+	 max_y = math.max(max_y, b:max_corner().y - b:min_corner().y)
+	 emit(s)
+      end
+   end
+end
+
+return laserslicer
