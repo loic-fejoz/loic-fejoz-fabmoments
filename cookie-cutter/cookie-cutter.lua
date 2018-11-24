8543df2868f809dcdb8c25039e0edcd21b73d5c9
Loïc Fejoz <loic@fejoz.net>
2018-11-24 14:49:03 +0100

    fix(cookie-cutter): Handle multiple paths

--- cookie-cutter/cookie-cutter.lua
+++ cookie-cutter/cookie-cutter.lua
@@ -4,29 +4,48 @@ local cookie_cutter_module = {}
 
 function cookie_cutter_module.svg_extrude_centered(filename, dpi, height)
   local svg_shapes = svg_ex(filename, dpi)
+  local shapes = {}
   for i, contour in pairs(svg_shapes) do
     obj = linear_extrude(
       v(0, 0, height),
       contour:outline())
     --obj = scale(-1, 1, 1) * obj
-    bb = bbox(obj):center()
-    obj = translate(-1 * bb) * obj
-    return obj
+      table.insert(shapes, obj)
+  end
+  local alls = union(shapes)
+  bb = bbox(alls):center()
+  for i, _ in pairs(shapes) do
+    shapes[i] = translate(-1 * bb) * shapes[i]
+  end
+  if table.getn(shapes) == 1 then
+    return shapes[1]
+  else
+    return shapes
   end
 end
 
 function cookie_cutter_module.dilate_it(shape, cfg)
+  if type(shape) == 'table' then
+    local results = {}
+    for i, _ in pairs(shape) do
+      results[i] = cookie_cutter_module.dilate_it(shape[i], cfg)
+    end
+    return union(results)
+  end
 --obj_bb = bbox(obj):extent()
 --factor = math.max(
 --  (obj_bb.x + 2*cfg.thickness) / obj_bb.x,
 --  (obj_bb.y + 2*cfg.thickness) / obj_bb.y)
   local factor = cfg.thickness
   local dilated = scale(1, 1, 3) * dilate(shape, factor)
-  local dilated_bb = bbox(dilated):extent()
+  local dilated_bb = bbox(dilated)
+  local dilated_bb_p1 = dilated_bb:center()
+  local dilated_bb_size = dilated_bb:extent()
   local cutter = difference(
     intersection{
       dilated,
-        ccube(dilated_bb.x + 3* cfg.thickness, dilated_bb.y + 3 * cfg.thickness, cfg.height)
+      translate(dilated_bb_p1.x, dilated_bb_p1.y, 0) * 
+        ccube(dilated_bb_size.x + 3* cfg.thickness, dilated_bb_size.y + 3 * cfg.thickness, cfg.height)
     },
     shape)
   return cutter
@@ -34,7 +53,7 @@ end
 
 function cookie_cutter_module.create(cfg)
   if cfg.filename == nil then 
-    error("filename is mandotary")
+    error("filename is mandatory")
   end
   if cfg.thickness == nil then
     cfg.thickness = 0.6
@@ -50,7 +69,6 @@ function cookie_cutter_module.create(cfg)
   end
   local obj = cookie_cutter_module.svg_extrude_centered(cfg.filename, 90, cfg.height)
   local cutter = cookie_cutter_module.dilate_it(obj, cfg)
-  --emit(cutter)
   local base_cfg = {}
   base_cfg.height = cfg.base
   base_cfg.thickness = 5
