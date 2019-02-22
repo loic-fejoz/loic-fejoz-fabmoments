ff9375c73b1e685bdabd10480e46ee31158b3be0
Loïc Fejoz <loic@fejoz.net>
2019-02-22 20:57:58 +0100

    feat(gallery): Add involute of circle

---
+++ icesl-gallery/involute.lua
@@ -0,0 +1,24 @@
+-- emit the inside of the involute of the circle of radius r for the first two quadrants
+local r = 5
+local h = 3
+involute_of_circle = implicit_solid(v(-6*r,0,0), v(2*r,4*r,h), 0.25,
+[[
+uniform float r = 5;
+float solid(vec3 p) {
+	float l = length(p.xy) - r;
+	if (l <= 0) {
+		return l;
+	}
+	float num = p.y + sqrt(p.x * p.x + p.y * p.y - r * r);
+	float denom = r + p.x;
+	float theta = 2 * atan(num, denom);
+	vec2 c = vec2(r * cos(theta), r * sin(theta));
+	float s = r * theta;
+	float sp = length(p.xy - c);
+	return sp - s;
+}
+]])
+set_uniform_scalar(involute_of_circle, 'r', r)
+emit(involute_of_circle,2)
+
+emit(cylinder(r,h),1)
