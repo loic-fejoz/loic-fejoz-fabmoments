89e089e61f79397be4590c95cb616cff1fc9441f
Loïc Fejoz <loic@fejoz.net>
2019-02-23 00:12:16 +0100

    fix(gallery): Update and compare gear
    Move it into its own function.
    Compare with gear provided by IceSL.
    Still on-going work.

--- icesl-gallery/gear.lua
+++ icesl-gallery/gear.lua
@@ -20,15 +20,90 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]
--- emit the inside of the involute of the circle of radius r for the first two quadrants
-local r = 15
-local n = 16
-local h = 3
-gear = implicit_distance_field(v(-6*r,-2*r,0), v(2*r,4*r,h),
+
+function gear_solid(params)
+	local number_of_teeth = params.number_of_teeth or 15
+	local circular_pitch = params.circular_pitch or -1
+	local diametral_pitch = params.diametral_pitch or -1
+	local pressure_angle = params.pressure_angle or 28
+	local clearance = params.clearance or 0.2
+	local gear_thickness = params.gear_thickness or 12
+	local rim_thickness = params.rim_thickness or 15
+	local rim_width = params.rim_width or 5
+	local hub_thickness = params.hub_thickness or 17
+	local hub_diameter = params.hub_diameter or 15
+	local bore_diameter = params.bore_diameter or 5
+	local circles = params.circles or 3
+	local backlash = params.backlash or 0
+	local twist = params.twist or 0
+	local involute_facets = params.involute_facets or 0
+	local flat = params.flat or false
+	
+	if ( circular_pitch < 0 and diametral_pitch < 0 ) then
+		print("!! gear module needs either a diametral_pitch or circular_pitch !!");
+		return Void
+	end
+
+	-- Convert diametrial pitch to our native circular pitch
+	if circular_pitch < 0 then
+		circular_pitch = 180/diametral_pitch
+	end
+	print("Circular pitch:" .. circular_pitch)
+
+	-- Pitch diameter: Diameter of pitch circle.
+	pitch_diameter  =  number_of_teeth * circular_pitch / 180;
+	pitch_radius = pitch_diameter/2;
+	print("Teeth:" .. number_of_teeth)
+	print("Pitch radius:" .. pitch_radius);
+
+	-- Base Circle
+	base_radius = pitch_radius*cos(pressure_angle);
+	print("Base radius:" .. base_radius) 
+
+	-- Diametrial pitch: Number of teeth per unit length.
+	pitch_diametrial = number_of_teeth / pitch_diameter;
+	print("Pitch diametrial:" .. pitch_diametrial)
+
+	-- Addendum: Radial distance from pitch circle to outside circle.
+	addendum = 1/pitch_diametrial;
+	print("Addendum:" .. addendum)
+
+	--Outer Circle
+	outer_radius = pitch_radius+addendum;
+	print("Outer radius:" .. outer_radius)
+
+	-- Dedendum: Radial distance from pitch circle to root diameter
+	dedendum = addendum + clearance;
+	print("Dedendum:" .. dedendum)
+
+	-- Root diameter: Diameter of bottom of tooth spaces.
+	root_radius = pitch_radius-dedendum;
+	print("Root radius:" .. root_radius)
+	backlash_angle = backlash / pitch_radius * 180 / math.pi;
+	print("Backlash angle:"..backlash_angle)
+	half_thick_angle = (360 / number_of_teeth - backlash_angle) / 4;
+	print("half_thick_angle:" .. half_thick_angle)
+
+	-- Variables controlling the rim.
+	rim_radius = root_radius - rim_width;
+	print("Rim radius:"..rim_radius)
+
+	-- Variables controlling the circular holes in the gear.
+	circle_orbit_diameter=hub_diameter/2+rim_radius;
+	circle_orbit_curcumference=math.pi*circle_orbit_diameter;
+
+
+	local gear = implicit_distance_field(
+		v(-2*root_radius,-2*root_radius,0),
+		v(2*root_radius,2*root_radius, rim_thickness*0.8),
 [[
 const float PI = 3.1415926535897932384626433832795;
 uniform float r = 5;
-uniform float n = 16;
+uniform float z = 16;
+uniform float r_outside = 20;
+uniform float r_root = 15;
+
+uniform float delta_involute = 0.5;
 
 float involute_solid(vec3 p) {
 
@@ -38,24 +113,38 @@ float involute_solid(vec3 p) {
 	vec2 c = vec2(r * cos(theta), r * sin(theta));
 	float s = r * theta;
 	float sp = length(p.xy - c);
-	return sp - s;
+	return sp - s + delta_involute;
 }
 
 float distance(vec3 p) {
-	float l = length(p.xy) - r;
+	float l_outside = length(p.xy) - r_outside;
+	float l_root = length(p.xy) - r_root;
 	
-	float alpha1 = mod(atan(p.y, p.x), 2 * PI / n);
+	float alpha1 = mod(atan(p.y, p.x), 2 * PI / z);
 	float rho = length(p.xy);
 	vec3 p1 = vec3(rho * cos(alpha1), rho * sin(alpha1), p.z);
 	
-	float alpha2 = mod(atan(p.y, -p.x), 2 * PI / n);
+	float alpha2 = mod(atan(p.y, -p.x), 2 * PI / z);
 	vec3 p2 = vec3(rho * cos(alpha2), rho * sin(alpha2), p.z);
 	
-	return min(l, max(involute_solid(p1), involute_solid(p2)));
+	return max(l_outside, min(l_root, max(involute_solid(p1), involute_solid(p2))));
 }
 ]])
-set_uniform_scalar(gear, 'r', r)
-set_uniform_scalar(gear, 'n', n)
-emit(gear,2)
+	set_uniform_scalar(gear, 'r', base_radius)
+	set_uniform_scalar(gear, 'r_outside',outer_radius)
+	set_uniform_scalar(gear, 'r_root',root_radius)
+	set_uniform_scalar(gear, 'z', number_of_teeth)
+	--set_uniform_scalar(gear, 'delta_involute', ???)
+	return gear
+end
 
-emit(cylinder(r,h),1)
+
+local params = {number_of_teeth=20,circular_pitch=300,flat=true}
+
+local b_solid = gear_solid(params)
+emit(b_solid,2)
+
+
+dofile("C:\\Users\\lfejoz\\AppData\\Roaming\\IceSL\\icesl-models\\libs\\gear.lua")
+local b = rotate(0,0, 180 / params.number_of_teeth) * gear(params)
+emit(b,3)
